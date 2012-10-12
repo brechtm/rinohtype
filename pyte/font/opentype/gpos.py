2b8de0e78ff02b9a31e2642c311d2403fdc2edf0
Brecht Machiels <brecht@mos6581.org>
2012-10-13 01:43:52 +0200

    significant speedup by reading Class2Records as structs

--- pyte/font/opentype/gpos.py
+++ pyte/font/opentype/gpos.py
@@ -1,10 +1,12 @@
 
+import struct
+
 from .tables import OpenTypeTable, MultiFormatTable, context_array, offset_array
 from .parse import fixed, int16, uint16, tag, glyph_id, offset, array, indirect
 from .parse import Packed
 from .layout import LayoutTable, ScriptListTable, FeatureListTable, LookupTable
 from .layout import Coverage, ClassDefinition
-
+from ...util import timed, cached_property
 
 class ValueFormat(Packed):
     reader = uint16
@@ -16,6 +18,48 @@ class ValueFormat(Packed):
               ('YPlaDevice', 0x0020, bool),
               ('XAdvDevice', 0x0040, bool),
               ('YAdvDevice', 0x0080, bool)]
+    formats = {'XPlacement': 'h',
+               'YPlacement': 'h',
+               'XAdvance': 'h',
+               'YAdvance': 'h',
+               'XPlaDevice': 'H',
+               'YPlaDevice': 'H',
+               'XAdvDevice': 'H',
+               'YAdvDevice': 'H'}
+
+    @cached_property
+    def data_format(self):
+        data_format = ''
+        for name, present in self.items():
+            if present:
+                data_format += self.formats[name]
+        return data_format
+
+    @cached_property
+    def value_record_type(self):
+        formats = {'XPlacement': int16,
+                   'YPlacement': int16,
+                   'XAdvance': int16,
+                   'YAdvance': int16,
+                   'XPlaDevice': offset,
+                   'YPlaDevice': offset,
+                   'XAdvDevice': offset,
+                   'YAdvDevice': offset}
+        entries = []
+        for name, present in self.items():
+            if present:
+                entries.append((name, formats[name]))
+
+        class ValueRecord(OpenTypeTable):
+            entries = entries
+            num_entries = len(entries)
+
+            def __init__(self, file, value_format):
+                super().__init__(file, None)
+                for name, present in value_format.items():
+                    if present:
+                        self[name] = self.formats[name](file)
+        return ValueRecord
 
 
 class SingleAdjustmentSubtable(OpenTypeTable):
@@ -23,10 +67,14 @@ class SingleAdjustmentSubtable(OpenTypeTable):
 
 
 class Class2Record(OpenTypeTable):
-    def __init__(self, file, format_1, format_2):
+    def __init__(self, file, record_struct, value_1_length):
         super().__init__(file)
-        self['Value1'] = ValueRecord(file, format_1)
-        self['Value2'] = ValueRecord(file, format_2)
+##        self['Value1'] = ValueRecord(file, format_1)
+##        self['Value2'] = ValueRecord(file, format_2)
+        data = record_struct.unpack(file.read(record_struct.size))
+        data_1, data_2 = data[:value_1_length], data[value_1_length:]
+        self['Value1'] = data_1
+        self['Value2'] = data_2
 
 
 # TODO: MultiFormatTable
@@ -41,14 +89,17 @@ class PairAdjustmentSubtable(MultiFormatTable):
                    ('Class1Count', uint16),
                    ('Class2Count', uint16)]}
 
+    @timed
     def __init__(self, file, file_offset):
         super().__init__(file, file_offset)
         format_1, format_2 = self['ValueFormat1'], self['ValueFormat2']
         if self['PosFormat'] == 1:
+            record_struct = struct.Struct('>' + format_1.data_format + format_2.data_format)
+            value_1_length = len(format_1)
             pst_reader = (lambda file, file_offset: PairSetTable(file,
                                                                  file_offset,
-                                                                 format_1,
-                                                                 format_2))
+                                                                 record_struct,
+                                                                 value_1_length))
             self['PairSet'] = (offset_array(pst_reader, 'PairSetCount')
                                    (self, file, file_offset))
         elif self['PosFormat'] == 2:
@@ -63,7 +114,8 @@ class PairAdjustmentSubtable(MultiFormatTable):
             except ValueError:
                 raise KeyError
             pair_value_record = self['PairSet'][index].by_second_glyph_id[b_id]
-            return pair_value_record['Value1']['XAdvance']
+##            return pair_value_record['Value1']['XAdvance']
+            return pair_value_record['Value1'][0]
         elif self['PosFormat'] == 2:
             a_class = self['ClassDef1'].class_number(a_id)
             b_class = self['ClassDef2'].class_number(b_id)
@@ -74,9 +126,11 @@ class PairAdjustmentSubtable(MultiFormatTable):
 class PairSetTable(OpenTypeTable):
     entries = [('PairValueCount', uint16)]
 
-    def __init__(self, file, file_offset, format_1, format_2):
+    def __init__(self, file, file_offset, record_struct, value_1_length):
         super().__init__(file, file_offset)
-        pvr_reader = lambda file: PairValueRecord(file, format_1, format_2)
+##        pair_value_record_size = 2 + record_struct.size
+##        buffer = file.read(pair_value_record_size * self['PairValueCount'])
+        pvr_reader = lambda file: PairValueRecord(file, record_struct, value_1_length)
         self['PairValueRecord'] = array(pvr_reader, self['PairValueCount'])(file)
         self.by_second_glyph_id = {}
         for record in self['PairValueRecord']:
@@ -87,21 +141,21 @@ class PairValueRecord(Class2Record):
     entries = [('SecondGlyph', glyph_id)]
 
 
-class ValueRecord(OpenTypeTable):
-    formats = {'XPlacement': int16,
-               'YPlacement': int16,
-               'XAdvance': int16,
-               'YAdvance': int16,
-               'XPlaDevice': offset,
-               'YPlaDevice': offset,
-               'XAdvDevice': offset,
-               'YAdvDevice': offset}
-
-    def __init__(self, file, value_format):
-        super().__init__(file, None)
-        for name, present in value_format.items():
-            if present:
-                self[name] = self.formats[name](file)
+##class ValueRecord(OpenTypeTable):
+##    formats = {'XPlacement': int16,
+##               'YPlacement': int16,
+##               'XAdvance': int16,
+##               'YAdvance': int16,
+##               'XPlaDevice': offset,
+##               'YPlaDevice': offset,
+##               'XAdvDevice': offset,
+##               'YAdvDevice': offset}
+##
+##    def __init__(self, file, value_format):
+##        super().__init__(file, None)
+##        for name, present in value_format.items():
+##            if present:
+##                self[name] = self.formats[name](file)
 
 
 class GposTable(LayoutTable):
