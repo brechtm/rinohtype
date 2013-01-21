c2547b040831983117a811fe98033ac20743f4ad
Brecht Machiels <brecht@mos6581.org>
2013-01-21 23:43:35 +0100

    more flowable docstrings

--- pyte/flowable.py
+++ pyte/flowable.py
@@ -1,9 +1,12 @@
 """
+Base classes for flowable and floating document elements. These are elements
+that make up the content of a document and are rendered onto its pages.
 
-* :class:`FlowableStyle`:
-* :class:`Flowable`:
-* :class:`Floating`: Decorator to transform a :class:`Flowable` into a float
-
+* :class:`Flowable`: Element that is rendered onto a :class:`Container`.
+* :class:`FlowableStyle`: Style class specifying the vertical space surrounding
+                          a :class:`Flowable`.
+* :class:`Floating`: Decorator to transform a :class:`Flowable` into a floating
+                     element.
 """
 
 
@@ -11,23 +14,41 @@ from .style import Style, Styled
 from .util import Decorator
 
 
+__all__ = ['Flowable', 'FlowableStyle', 'Floating']
+
+
 class FlowableStyle(Style):
+    """The :class:`Style` for :class:`Flowable` objects. It has the following
+    attributes:
+
+    * `space_above`: Vertical space preceding the flowable (:class:`Dimension`)
+    * `space_below`: Vertical space following the flowable (:class:`Dimension`)
+    """
+
     attributes = {'space_above': 0,
                   'space_below': 0}
 
 
 class Flowable(Styled):
-    """"""
+    """An element that can be 'flowed' into a :class:`Container`. A flowable can
+    adapt to the width of the container, or it can horizontally align itself in
+    the container."""
 
     style_class = FlowableStyle
 
     def __init__(self, style=None):
+        """Initialize this flowable and associate it with the given `style`."""
         super().__init__(style)
         self.resume = False
 
     def flow(self, container):
         """Flow this flowable into `container` and return the vertical space
-        consumed."""
+        consumed.
+
+        The flowable's contents is preceded by a vertical space with a height
+        as specified in its style's `space_above` attribute. Similarly, the
+        flowed content is followed by a vertical space with a height given
+        by the `space_below` style attribute."""
         start_offset = container.cursor
         if not self.resume:
             self.resume = True
@@ -38,6 +59,8 @@ class Flowable(Styled):
         return container.cursor - start_offset
 
     def render(self, container):
+        """Renders the flowable's content to `container`. This is different for
+        each type of flowable."""
         raise NotImplementedError
 
 
