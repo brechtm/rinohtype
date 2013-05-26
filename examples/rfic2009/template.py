68ac82209fcc890149943f18a96e4644dd569b48
Brecht Machiels <brecht@mos6581.org>
2013-05-26 10:14:40 +0200

    separately report setup and render time

--- examples/rfic2009/template.py
+++ examples/rfic2009/template.py
@@ -12,10 +12,16 @@ from citeproc.source.bibtex import BibTeX
 bib_source = BibTeX('references.bib')
 
 doc = RFIC2009Paper('template.xml', bib_source)
+
+setup_time = time.clock()
+print('Setup time: {:.2f} seconds'.format(setup_time)) # parsing XML and fonts
+
 doc.render('template')
 
 run_time = time.clock()
-
-print('Total execution time: {} seconds'.format(run_time))
+render_time = run_time - setup_time
+print('Render time: {:.2f} seconds ({:.2f}s per page)'
+      .format(render_time, render_time / doc.page_count))
+print('Total execution time: {:.2f} seconds'.format(run_time))
 
 #os.system("ps2pdf.cmd")
