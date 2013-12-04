8c1b3b95fa173210dbbc38da76b58f1085bcdbf2
Brecht Machiels <brecht@mos6581.org>
2013-12-04 13:58:44 +0100

    Remove PSG from the repository
    The PSG backend is broken and unlikely to be updated any time
    soon.

--- orm2/orm2/adapters/mysql/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: iso-8859-1 -*-
-
-"""
-orm's U{MySQL <http://www.mysql.org/>} adapter.
-"""
--- orm2/orm2/build/lib/orm2/adapters/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: iso-8859-1 -*-
-
-"""
-Module for orm2's database 'adapters', wrapper classes for specific
-RDBMS interfaces, and also additional datatypes and so on.
-"""
