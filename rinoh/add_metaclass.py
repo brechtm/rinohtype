9e2e178e7cf85a8cf32e9c08a7814b3083d5eaa1
Brecht Machiels <brecht@mos6581.org>
2014-03-01 17:12:48 +0100

    Copy add_metaclass from the 'six' module
    This removes the need for six to be installed.

---
+++ rinoh/add_metaclass.py
@@ -0,0 +1,16 @@
+# from the 'six' module by Benjamin Peterson
+
+def add_metaclass(metaclass):
+    """Class decorator for creating a class with a metaclass."""
+    def wrapper(cls):
+        orig_vars = cls.__dict__.copy()
+        orig_vars.pop('__dict__', None)
+        orig_vars.pop('__weakref__', None)
+        slots = orig_vars.get('__slots__')
+        if slots is not None:
+            if isinstance(slots, str):
+                slots = [slots]
+            for slots_var in slots:
+                orig_vars.pop(slots_var)
+        return metaclass(cls.__name__, cls.__bases__, orig_vars)
+    return wrapper
--- rinoh/dimension.py
+++ rinoh/dimension.py
@@ -24,7 +24,7 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 from rinoh.py2compat import *
 
-from six import add_metaclass
+from .add_metaclass import add_metaclass
 
 
 __all__ = ['Dimension', 'PT', 'INCH', 'MM', 'CM']
