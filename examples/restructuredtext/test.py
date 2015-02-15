685da17e9cca821dc96fb8ca798eca64f5ebda05
Brecht Machiels <brecht@mos6581.org>
2015-02-15 21:18:57 +0100

    Use the global Manual class for the reST example

--- examples/restructuredtext/test.py
+++ examples/restructuredtext/test.py
@@ -1,8 +1,14 @@
-from rst import ReStructuredTextDocument, STYLESHEET
+from rinoh.backend import pdf
+from rinoh.frontend.rst import ReStructuredTextParser
 
+from rinohlib.stylesheets.somestyle import stylesheet as STYLESHEET
+from rinohlib.templates.manual import Manual, ManualOptions
 
 if __name__ == '__main__':
 #    for name in ('quickstart', 'FAQ', 'THANKS'):
     for name in ('optionlist', ):
-        document = ReStructuredTextDocument(name + '.txt', STYLESHEET)
+        parser = ReStructuredTextParser()
+        document_tree = parser.parse(name + '.txt')
+        manual_options = ManualOptions(stylesheet=STYLESHEET)
+        document = Manual(document_tree, options=manual_options, backend=pdf)
         document.render(name)
