704e19a53bca1f9cddfda2280037d9e64c84f9da
Brecht Machiels <brecht@mos6581.org>
2015-04-11 13:16:53 +0200

    rinoh: print help when no filename is given

--- rinoh/tool.py
+++ rinoh/tool.py
@@ -12,16 +12,20 @@ from rinohlib.templates.article import Article, ArticleOptions
 
 def main():
     parser = argparse.ArgumentParser(description='Render a reStructuredText '
-                                                 'document.')
+                                                 'document to PDF.')
     parser.add_argument('input', type=str, nargs='?',
                        help='the reStructuredText document to render')
     parser.add_argument('--paper', type=str, nargs='?', default='A4',
                        help='the paper size to render to')
     args = parser.parse_args()
 
-    input_dir, input_file = os.path.split(args.input)
-    input_base, input_ext = os.path.splitext(input_file)
+    try:
+        input_dir, input_file = os.path.split(args.input)
+    except AttributeError:
+        parser.print_help()
+        return
 
+    input_base, input_ext = os.path.splitext(input_file)
     if input_dir:
         os.chdir(input_dir)
     parser = ReStructuredTextParser()
