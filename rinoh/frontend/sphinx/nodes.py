827dbe85ffd40e505375fa09e9584c58b68dc0ff
Brecht Machiels <brecht@mos6581.org>
2015-04-26 10:33:56 +0200

    Basic support for more Sphinx custom docutils nodes
    Allows rendering of the Sphinx docs.
    (if TableStyle.split_minimum_rows is set to 0)

--- rinoh/frontend/sphinx/nodes.py
+++ rinoh/frontend/sphinx/nodes.py
@@ -8,13 +8,18 @@
 
 from ...decoration import Framed
 from ...flowable import DummyFlowable, PageBreak
+from ...paragraph import Paragraph
+from ...structure import DefinitionList, DefinitionTerm
 from ...text import SingleStyledText
-from ..rst import BodyElement, BodySubElement, InlineElement, GroupingElement
-from ..rst.nodes import AdmonitionBase
+from ..rst import BodyElement, InlineElement, GroupingElement
+from ..rst.nodes import AdmonitionBase, Strong, Inline
 
 
 __all__ = ['Compact_Paragraph', 'Index', 'SeeAlso', 'Glossary', 'Start_of_File',
-           'Todo_Node', 'Raw']
+           'Todo_Node', 'Raw', 'HighlightLang', 'Literal_Strong',
+           'Desc', 'Desc_Signature', 'Desc_Name', 'Desc_ParameterList',
+           'Desc_Parameter', 'Desc_Optional', 'Desc_Content',
+           'VersionModified', 'Tabular_Col_Spec', 'AutoSummary_Table']
 
 
 class Compact_Paragraph(GroupingElement):
@@ -31,6 +36,10 @@ class Index(BodyElement, InlineElement):
 
 class SeeAlso(AdmonitionBase):
     title = 'See also'
+
+
+class VersionModified(GroupingElement):
+    pass
 
 
 class Glossary(GroupingElement):
@@ -52,3 +61,54 @@ class Raw(BodyElement):
             return PageBreak()
         else:
             return DummyFlowable()
+
+
+class HighlightLang(BodyElement):
+    def build_flowable(self):
+        return DummyFlowable()
+
+
+class Literal_Strong(Strong):
+    pass
+
+
+class Desc(BodyElement):
+    def build_flowable(self):
+        return DefinitionList([(self.desc_signature.flowable(),
+                                self.desc_content.flowable())])
+
+
+class Desc_Signature(BodyElement):
+    def build_flowable(self):
+        return Paragraph(self.desc_name.styled_text())
+
+
+class Desc_Name(Inline):
+    pass
+
+
+class Desc_ParameterList(InlineElement):
+    def build_styled_text(self):
+        return [parameter.process() for parameter in self.desc_parameter]
+
+
+class Desc_Parameter(Inline):
+    pass
+
+
+class Desc_Optional(Desc_ParameterList):
+    pass
+
+
+class Desc_Content(GroupingElement):
+    pass
+
+
+class Tabular_Col_Spec(BodyElement):
+    def build_flowable(self):
+        return DummyFlowable()
+
+
+class AutoSummary_Table(GroupingElement):
+    def build_flowable(self):
+        return DummyFlowable()
