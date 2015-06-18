74586b9055772b43d7333710686a4904d9737d6a
Brecht Machiels <brecht@mos6581.org>
2015-06-18 21:57:17 +0200

    JPEGReader: we can stop after the Start-of-Frame

--- rinoh/backend/pdf/jpeg.py
+++ rinoh/backend/pdf/jpeg.py
@@ -26,7 +26,6 @@ def create_reader(data_format, process_struct=lambda data: data[0]):
 
 class JPEGReader(XObjectImage):
     def __init__(self, file_or_filename):
-        print('opening', file_or_filename)
         try:
             self._file = open(file_or_filename, 'rb')
             self.filename = file_or_filename
@@ -56,35 +55,15 @@ class JPEGReader(XObjectImage):
                 marker = self.read_uchar()
             if prefix != 0xFF or marker == 0x00:
                 raise ValueError('Invalid or corrupt JPEG file')
-            print(hex(marker), 'at', hex(self._file.tell()), end=' ')
-            if marker == 0xD9:
-                print('end of image')
-                data_after_eoi = self._file.read()
-                if len(data_after_eoi):
-                    print('data after EOI!')
-                    print(data_after_eoi)
-                break
             header_length = self.read_ushort()
             if (marker & 0xF0) == 0xC0 and marker not in (0xC4, 0xC8, 0xCC):
                 v_size, h_size, bits_per_component, num_components = \
                     self._parse_start_of_frame(header_length)
+                break
             elif marker == 0xE0:
                 self._parse_jfif_segment(header_length)
             else:
                 self._file.seek(header_length - 2, SEEK_CUR)
-                if marker == 0xDA:
-                    print('SOS (followed by compressed data)')
-                    while True:
-                        if self._file.read(1) == b'\xff':
-                            byte = self.read_uchar()
-                            if 0xD0 <= byte <= 0xD7:
-                                print(hex(marker), 'at', hex(self._file.tell()),
-                                      'restart marker')
-                            elif byte != 0x00:  # escaped 0xFF
-                                self._file.seek(-2, SEEK_CUR)
-                                break
-                else:
-                    print('')
         return h_size, v_size, bits_per_component, num_components
 
     SOF_HEADER = create_reader('B H H B', lambda tuple: tuple)
@@ -92,7 +71,6 @@ class JPEGReader(XObjectImage):
     def _parse_start_of_frame(self, header_length):
         resume_position = self._file.tell() + header_length - 2
         (sample_precision, v_size, h_size, num_components) = self.SOF_HEADER()
-        print('SOF', sample_precision, v_size, h_size, num_components)
         self._file.seek(resume_position)
         return v_size, h_size, sample_precision, num_components
 
@@ -102,6 +80,5 @@ class JPEGReader(XObjectImage):
         (identifier, version, units,
          h_density, v_density, h_thumbnail, v_thumbnail) = self.JFIF_HEADER()
         assert identifier == b'JFIF\0'
-        print('JFIF', version, units, h_density, v_density)
         thumbnail_size = 3 * h_thumbnail * v_thumbnail
         assert header_length == 16 + thumbnail_size
