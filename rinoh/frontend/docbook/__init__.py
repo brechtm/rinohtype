98de8ef26abf48d5c86635eef93788ecdcbb3a5d
Brecht Machiels <brecht@mos6581.org>
2015-12-22 09:57:15 +0100

    Extract common frontend code (3)
    ElementTreeNode: common class shared by all XML formats (etree)

--- rinoh/frontend/docbook/__init__.py
+++ rinoh/frontend/docbook/__init__.py
@@ -9,99 +9,13 @@ import re
 
 from itertools import chain
 
-from .. import TreeNode
-
-from ..xml import elementtree
+from ..xml import elementtree, ElementTreeNode
 
 from ...flowable import StaticGroupedFlowables
-from ...text import MixedStyledText
 
 
-DOCBOOK_NS = 'http://docbook.org/ns/docbook'
-
-RE_WHITESPACE = re.compile('[\t\r\n ]+')
-
-
-def strip_namespace(tag):
-    if '{' in tag:
-        assert tag.startswith('{{{}}}'.format(DOCBOOK_NS))
-        return tag[tag.find('}') + 1:]
-    else:
-        return tag
-
-
-def filter(text, strip_leading_whitespace):
-    if text:
-        yield text, str(text).endswith(' ')
-
-
-def strip_and_filter(text, strip_leading_whitespace):
-    if strip_leading_whitespace:
-        text = text.lstrip()
-    for item, strip_leading_whitespace in filter(text,
-                                                 strip_leading_whitespace):
-        yield item, strip_leading_whitespace
-
-
-class DocBookNode(TreeNode):
-    @staticmethod
-    def node_tag_name(node):
-        return strip_namespace(node.tag)
-
-    @staticmethod
-    def node_parent(node):
-        return node._parent
-
-    @staticmethod
-    def node_children(node):
-        return node.getchildren()
-
-    @property
-    def _location(self):
-        return self.node.filename, self.node.sourceline, self.tag_name
-
-    @property
-    def text(self):
-        if self.node.text:
-            if self.get('xml:space') == 'preserve':
-                return self.node.text
-            else:
-                return RE_WHITESPACE.sub(' ', self.node.text)
-        else:
-            return ''
-
-    @property
-    def tail(self):
-        if self.node.tail:
-            return RE_WHITESPACE.sub(' ', self.node.tail)
-        else:
-            return None
-
-    @property
-    def attributes(self):
-        return self.node.attrib
-
-    def get(self, key, default=None):
-        return self.node.get(key, default)
-
-    def __getitem__(self, name):
-        return self.node[name]
-
-    def process_content(self, strip_leading_whitespace=True, style=None):
-        text_items = self._filter_whitespace(strip_leading_whitespace)
-        return MixedStyledText([item for item in text_items], style=style)
-
-    def _filter_whitespace(self, strip_leading_ws):
-        for item, strip_leading_ws in strip_and_filter(self.text,
-                                                       strip_leading_ws):
-            yield item
-        for child in self.getchildren():
-            child_text = child.styled_text(strip_leading_ws)
-            for item, strip_leading_ws in filter(child_text, strip_leading_ws):
-                yield item
-            for item, strip_leading_ws in strip_and_filter(child.tail,
-                                                           strip_leading_ws):
-                yield item
+class DocBookNode(ElementTreeNode):
+    NAMESPACE = 'http://docbook.org/ns/docbook'
 
 
 class BodyElementBase(DocBookNode):
@@ -157,7 +71,7 @@ from . import nodes
 
 class DocBookReader(object):
     rngschema = None
-    namespace = DOCBOOK_NS
+    namespace = DocBookNode.NAMESPACE
 
     def parse(self, file):
         filename = getattr(file, 'name', None)
--- rinoh/frontend/xml/__init__.py
+++ rinoh/frontend/xml/__init__.py
@@ -12,12 +12,104 @@ import re
 from urllib.parse import urljoin
 from urllib.request import pathname2url
 
+from ...text import MixedStyledText
+from ...util import NotImplementedAttribute
+
 from ... import DATA_PATH
+
+from .. import TreeNode
 
 
 CATALOG_PATH = os.path.join(DATA_PATH, 'xml', 'catalog')
 CATALOG_URL = urljoin('file:', pathname2url(CATALOG_PATH))
 CATALOG_NS = "urn:oasis:names:tc:entity:xmlns:xml:catalog"
+
+
+RE_WHITESPACE = re.compile('[\t\r\n ]+')
+
+
+def filter(text, strip_leading_whitespace):
+    if text:
+        yield text, str(text).endswith(' ')
+
+
+def strip_and_filter(text, strip_leading_whitespace):
+    if strip_leading_whitespace:
+        text = text.lstrip()
+    for item, strip_leading_whitespace in filter(text,
+                                                 strip_leading_whitespace):
+        yield item, strip_leading_whitespace
+
+
+class ElementTreeNode(TreeNode):
+    NAMESPACE = NotImplementedAttribute()
+
+    @classmethod
+    def strip_namespace(cls, tag):
+        if '{' in tag:
+            assert tag.startswith('{{{}}}'.format(cls.NAMESPACE))
+            return tag[tag.find('}') + 1:]
+        else:
+            return tag
+
+    @classmethod
+    def node_tag_name(cls, node):
+        return cls.strip_namespace(node.tag)
+
+    @staticmethod
+    def node_parent(node):
+        return node._parent
+
+    @staticmethod
+    def node_children(node):
+        return node.getchildren()
+
+    @property
+    def _location(self):
+        return self.node.filename, self.node.sourceline, self.tag_name
+
+    @property
+    def text(self):
+        if self.node.text:
+            if self.get('xml:space') == 'preserve':
+                return self.node.text
+            else:
+                return RE_WHITESPACE.sub(' ', self.node.text)
+        else:
+            return ''
+
+    @property
+    def tail(self):
+        if self.node.tail:
+            return RE_WHITESPACE.sub(' ', self.node.tail)
+        else:
+            return None
+
+    @property
+    def attributes(self):
+        return self.node.attrib
+
+    def get(self, key, default=None):
+        return self.node.get(key, default)
+
+    def __getitem__(self, name):
+        return self.node[name]
+
+    def process_content(self, strip_leading_whitespace=True, style=None):
+        text_items = self._filter_whitespace(strip_leading_whitespace)
+        return MixedStyledText([item for item in text_items], style=style)
+
+    def _filter_whitespace(self, strip_leading_ws):
+        for item, strip_leading_ws in strip_and_filter(self.text,
+                                                       strip_leading_ws):
+            yield item
+        for child in self.getchildren():
+            child_text = child.styled_text(strip_leading_ws)
+            for item, strip_leading_ws in filter(child_text, strip_leading_ws):
+                yield item
+            for item, strip_leading_ws in strip_and_filter(child.tail,
+                                                           strip_leading_ws):
+                yield item
 
 
 def element_factory(xml_frontend):
