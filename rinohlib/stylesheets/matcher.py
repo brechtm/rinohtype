5bc69c9c427528b023b80d70342bfe99f3953ccd
Brecht Machiels <brecht@mos6581.org>
2016-04-06 16:22:21 +0200

    Avoid duplication in the default Matcher

--- rinohlib/stylesheets/matcher.py
+++ rinohlib/stylesheets/matcher.py
@@ -10,6 +10,9 @@ __all__ = ['matcher']
 
 matcher = StyledMatcher()
 
+
+# title page items
+
 matcher('title page title', Paragraph.like('title page title'))
 matcher('title page subtitle', Paragraph.like('title page subtitle'))
 matcher('title page author', Paragraph.like('title page author'))
@@ -18,45 +21,44 @@ matcher('title page extra', Paragraph.like('title page extra'))
 
 matcher('body', Paragraph)
 
+
+# inline text
+
 matcher('italic', StyledText.like('italic'))
-
 matcher('bold', StyledText.like('bold'))
-
 matcher('emphasis', StyledText.like('emphasis'))
-
 matcher('strong', StyledText.like('strong'))
-
 matcher('quote', StyledText.like('quote'))
-
 matcher('file path', StyledText.like('file path'))
-
 matcher('window title', StyledText.like('window title'))
-
 matcher('UI control', StyledText.like('UI control'))
-
 matcher('menu cascade', StyledText.like('menu cascade'))
-
 matcher('draft comment', StyledText.like('draft comment'))
-
 matcher('title reference', StyledText.like('title reference'))
-
 matcher('monospaced', StyledText.like('monospaced'))
-
 matcher('error', StyledText.like('error'))
+
+matcher('linked reference', Reference.like(link=True))
+matcher('unlinked reference', Reference.like(link=False))
 
 matcher('internal hyperlink', StyledText.like('internal link'))
 matcher('external hyperlink', StyledText.like('external link'))
 matcher('broken hyperlink', StyledText.like('broken link'))
 
-matcher('literal', Paragraph.like('literal'))
 
-matcher('block quote', GroupedFlowables.like('block quote'))
+# paragraphs
+
+matcher('literal', Paragraph.like('literal'))
 
 matcher('attribution', Paragraph.like('attribution'))
 
+matcher('block quote', GroupedFlowables.like('block quote'))
 matcher('line block', GroupedFlowables.like('line block'))
 matcher('nested line block', 'line block' / SelectorByName('line block'))
 matcher('line block line', Paragraph.like('line block line'))
+
+
+#
 
 matcher('title', Paragraph.like('title'))
 matcher('subtitle', Paragraph.like('subtitle'))
@@ -70,25 +72,19 @@ for i in range(1, 6):
     matcher('heading level {}'.format(i), Heading.like(level=i))
     matcher('unnumbered heading level {}'.format(i),
             Heading.like('unnumbered', level=i))
-
 matcher('other heading levels', Heading)
 
 matcher('appendix', Section.like('appendix'))
-
 for i in range(1, 6):
     matcher('appendix heading level {}'.format(i),
             'appendix' / Heading.like(level=i))
 
-matcher('topic', GroupedFlowables.like('topic'))
-
 matcher('prerequisites', GroupedFlowables.like('prerequisites'))
-
 matcher('prerequisites title', 'prerequisites' / Paragraph.like('title'))
 
 matcher('post requirement', GroupedFlowables.like('post requirement'))
 
 matcher('abstract', GroupedFlowables.like('abstract'))
-
 matcher('abstract paragraph', 'abstract' / Paragraph)
 
 matcher('example', GroupedFlowables.like('example'))
@@ -104,81 +100,66 @@ matcher('sidebar frame', GroupedFlowables.like('sidebar'))
 matcher('sidebar title', 'sidebar' / Paragraph.like('title'))
 matcher('sidebar subtitle', 'sidebar' / Paragraph.like('subtitle'))
 
+
+# lists
+
 matcher('list item label', ListItemLabel)
 
-enum_list = List.like('enumerated')
-matcher('enumerated list', enum_list)
-matcher('enumerated list item', enum_list / ListItem)
-matcher('enumerated list item label', enum_list / ListItem / ListItemLabel)
+matcher('enumerated list', List.like('enumerated'))
+matcher('enumerated list item', 'enumerated list' / ListItem)
+matcher('enumerated list item label', 'enumerated list item' / ListItemLabel)
+matcher('nested enumerated list', ListItem / 'enumerated list')
 
-matcher('nested enumerated list', ListItem / enum_list)
+matcher('(table) enumerated list', TableCell / ... / 'enumerated list')
+matcher('(table) enumerated list item', '(table) enumerated list' / ListItem)
+matcher('(table) enumerated list item label', '(table) enumerated list item'
+                                              / ListItemLabel)
 
-tbl_enum_list = TableCell / ... / enum_list
-matcher('(table) enumerated list', tbl_enum_list)
-matcher('(table) enumerated list item', tbl_enum_list / ListItem)
-matcher('(table) enumerated list item label', tbl_enum_list
-                                             / ListItem / ListItemLabel)
+matcher('bulleted list', List.like('bulleted'))
+matcher('bulleted list item', 'bulleted list' / ListItem)
+matcher('bulleted list item label', 'bulleted list item' / ListItemLabel)
+matcher('nested bulleted list', ListItem / 'bulleted list')
 
-bul_list = List.like('bulleted')
-matcher('bulleted list', bul_list)
-matcher('bulleted list item', bul_list / ListItem)
-matcher('bulleted list item label', bul_list / ListItem / ListItemLabel)
-
-matcher('nested bulleted list', ListItem / bul_list)
-
-tbl_bul_list = TableCell / ... / bul_list
-matcher('(table) bulleted list', tbl_bul_list)
-matcher('(table) bulleted list item', tbl_bul_list / ListItem)
-matcher('(table) bulleted list item label', tbl_bul_list
-                                            / ListItem / ListItemLabel)
+matcher('(table) bulleted list', TableCell / ... / 'bulleted list')
+matcher('(table) bulleted list item', '(table) bulleted list' / ListItem)
+matcher('(table) bulleted list item label', '(table) bulleted list item'
+                                            / ListItemLabel)
 
 matcher('steps list', List.like('steps'))
-matcher('steps list title', List.like('steps') / Paragraph.like('title'))
-matcher('steps list item label', List.like('steps')
-                                 / ListItem / ListItemLabel)
+matcher('steps list title', 'steps list' / Paragraph.like('title'))
+matcher('steps list item', 'steps list' / ListItem)
+matcher('steps list item label', 'steps list item' / ListItemLabel)
 
 matcher('choices list', List.like('choices'))
-
-matcher('choices list item label', List.like('choices')
-                                   / ListItem / ListItemLabel)
+matcher('choices list item', 'choices list' / ListItem)
+matcher('choices list item label', 'choices list item' / ListItemLabel)
 
 matcher('list item body', ListItem / GroupedFlowables)
-
 matcher('list item paragraph', ListItem / ... / Paragraph)
 
 matcher('definition list', DefinitionList)
-
 matcher('definition term', DefinitionTerm)
-
-matcher('definition term paragraph', DefinitionTerm / ... / Paragraph)
-
-matcher('definition term classifier',
-        DefinitionTerm / ... /StyledText.like('classifier'))
-
+matcher('definition term paragraph', SelectorByName('definition term')
+                                     / ... / Paragraph)
+matcher('definition term classifier', SelectorByName('definition term')
+                                      / ... /StyledText.like('classifier'))
 matcher('definition', Definition)
 
-matcher('linked reference', Reference.like(link=True))
-matcher('unlinked reference', Reference.like(link=False))
+matcher('related links', GroupedFlowables.like('related links'))
+matcher('related links section title', 'related links'
+                                       / Paragraph.like('title'))
+matcher('related links list', 'related links' / List)
+matcher('related links list item', 'related links list' / ListItem)
+matcher('related links list item label', 'related links list item'
+                                         / ListItemLabel)
+matcher('related links list item paragraph',
+        SelectorByName('related links list item') / ... / ReferencingParagraph)
+_ = SelectorByName('related links list item paragraph') / ...
+matcher('related link title reference', _ / ReferenceField.like(type=TITLE))
+matcher('related link page reference', _ / ReferenceField.like(type=PAGE))
+matcher('related link number reference', _ /ReferenceField.like(type=NUMBER))
+matcher('related link reference', _ / ReferenceField.like(type=REFERENCE))
 
-related_links = GroupedFlowables.like('related links')
-related_links_list = related_links / List
-related_links_list_item = related_links_list / ListItem
-related_links_list_item_paragraph = (related_links_list_item / ...
-                                     / ReferencingParagraph)
-matcher('related links', related_links)
-matcher('related links section title', related_links / Paragraph.like('title'))
-matcher('related links list', related_links_list)
-matcher('related links list item', related_links_list_item)
-matcher('related links list item label', related_links_list_item / ListItemLabel)
-matcher('related links list item paragraph', related_links_list_item_paragraph)
-matcher('related link title reference', related_links_list_item_paragraph / ...
-                                        / ReferenceField.like(type=TITLE))
-matcher('related link page reference', related_links_list_item_paragraph / ...
-                                       / ReferenceField.like(type=PAGE))
-matcher('related link number reference', related_links_list_item_paragraph / ...
-                                         / ReferenceField.like(type=NUMBER))
-matcher('related link reference', related_links_list_item_paragraph / ...
-                                  / ReferenceField.like(type=REFERENCE))
 
 # (Sphinx) version added/changed & deprecated
 
@@ -189,46 +170,29 @@ matcher('versionmodified', StyledText.like(classes=['versionmodified']))
 desc = DefinitionList.like('object description')
 
 matcher('object description', desc)
-
 matcher('object signature', desc / DefinitionTerm / ... / Paragraph)
-
 matcher('object name', desc / ... / StyledText.like('main object name'))
-
 matcher('additional name part', desc / ... / StyledText.like('additional name part'))
-
 matcher('object type', desc / ... / StyledText.like('type'))
-
 matcher('object returns', desc / ... / StyledText.like('returns'))
-
 matcher('object parentheses', desc / ... / StyledText.like('parentheses'))
-
 matcher('object parameter list', desc / ... / StyledText.like('parameter list'))
-
 matcher('object parameter', desc / ... / StyledText.like('parameter'))
-
 matcher('object parameter (no emphasis)', desc / ... / StyledText.like('noemph parameter'))
-
 matcher('object brackets', desc / ... / StyledText.like('brackets'))
-
 matcher('object optional parameter', desc / ... / StyledText.like('optional'))
-
 matcher('object annotation', desc / ... / StyledText.like('annotation'))
-
 matcher('object description content', desc / GroupedFlowables)
 
 
 # (Sphinx) production list
 
 matcher('production list', FieldList.like('production list'))
-
-matcher('production',
-        FieldList.like('production list') / LabeledFlowable.like('production'))
-
-matcher('token name',
-        FieldList.like('production list') / ... / Paragraph.like('token'))
-
-matcher('token definition',
-        FieldList.like('production list') / ... / Paragraph.like('definition'))
+matcher('production', 'production list' / LabeledFlowable.like('production'))
+matcher('token name', SelectorByName('production list')
+                      / ... / Paragraph.like('token'))
+matcher('token definition', SelectorByName('production list')
+                            / ... / Paragraph.like('definition'))
 
 
 # field lists
@@ -239,17 +203,13 @@ matcher('field name', Paragraph.like('field_name'))
 # option lists
 
 matcher('option', Paragraph.like('option_group'))
-
 matcher('option string', MixedStyledText.like('option_string'))
-
 matcher('option argument', MixedStyledText.like('option_arg'))
 
 matcher('admonition', Admonition)
-
-matcher('admonition title', Admonition / Paragraph.like('title'))
-
-matcher('admonition inline title', Admonition / ...
-                                   / StyledText.like('inline title'))
+matcher('admonition title', 'admonition' / Paragraph.like('title'))
+matcher('admonition inline title', SelectorByName('admonition')
+                                   / ... / StyledText.like('inline title'))
 
 for admonition_type in ('attention', 'caution', 'danger', 'error', 'warning'):
     admonition_selector = Admonition.like(admonition_type=admonition_type)
@@ -258,162 +218,116 @@ for admonition_type in ('attention', 'caution', 'danger', 'error', 'warning'):
     matcher(admonition_type + ' admonition inline title',
             admonition_selector / ... / StyledText.like('inline title'))
 
+# page header and footer
+
 matcher('header', Header)
-
 matcher('line under header', HorizontalRule.like('header'))
-
 matcher('footer', Footer)
-
 matcher('line above footer', HorizontalRule.like('footer'))
 
 
+# footnotes & citations
+
 matcher('footnote marker', NoteMarkerBase.like('footnote'))
-
 matcher('citation marker', NoteMarkerBase.like('citation'))
-
 matcher('footnote paragraph', Note / GroupedFlowables / Paragraph)
-
 matcher('footnote label', Note / Paragraph)
 
+
+# images & figures
+
 matcher('figure', Figure)
-
-
 matcher('image', Image)
-
 matcher('inline image', InlineImage)
-
 matcher('caption', Caption)
+matcher('figure legend', 'figure' / GroupedFlowables.like('legend'))
+matcher('figure legend paragraph', 'figure legend' / Paragraph)
 
-matcher('figure legend', Figure / GroupedFlowables.like('legend'))
 
-matcher('figure legend paragraph', Figure
-                                   / GroupedFlowables.like('legend')
-                                   / Paragraph)
+# front matter
 
 matcher('front matter section', FrontMatter > Section.like(level=1))
-
-matcher('front matter section heading', FrontMatter > Section.like(level=1) / Heading)
+matcher('front matter section heading', 'front matter section' / Heading)
 
 matcher('table of contents section', Section.like('table of contents'))
-
-matcher('table of contents title',
-        Section.like('table of contents', level=1) / Heading)
-
+matcher('table of contents title', 'table of contents section' / Heading)
 matcher('table of contents', TableOfContents)
-
 matcher('toc level 1', TableOfContentsEntry.like(depth=1))
-
 matcher('toc level 2', TableOfContentsEntry.like(depth=2))
-
 matcher('toc level 3', TableOfContentsEntry.like(depth=3))
-
 matcher('L3 toc level 3', TableOfContents.like(level=2)
                           / TableOfContentsEntry.like(depth=3))
 
-matcher('table with caption', TableWithCaption)
+# tables
 
 matcher('table', Table)
-
+matcher('table with caption', TableWithCaption)
 matcher('choices table', Table.like('choice'))
-
 matcher('table cell', Table / TableSection / TableRow / TableCell)
-
 matcher('table body cell background on even row',
-        TableBody
-        / TableRow
+        TableBody / TableRow
         / TableCell.like(row_index=slice(0, None, 2), rowspan=1)
         / TableCellBackground)
-
 matcher('table body cell background on odd row',
-        TableBody
-        / TableRow
+        TableBody / TableRow
         / TableCell.like(row_index=slice(0, None, 1), rowspan=1)
         / TableCellBackground)
-
-matcher('table body cell paragraph',
-        TableBody / TableRow / TableCell / ... / Paragraph)
-
+matcher('table body cell paragraph', SelectorByName('table cell')
+                                     / ... / Paragraph)
 matcher('table first column paragraph',
         TableBody / TableRow / TableCell.like(column_index=0) / ... / Paragraph)
-
-matcher('table body cell list item number',
-        TableBody / TableRow / TableCell / ... / ListItem / Paragraph)
-
+matcher('table body cell list item number', SelectorByName('table cell')
+                                            / ... / ListItem / Paragraph)
 matcher('table head cell', Table / TableHead / TableRow / TableCell)
-
-matcher('table head cell paragraph',
-        TableHead / TableRow / TableCell / Paragraph)
-
+matcher('table head cell paragraph', 'table head cell' / Paragraph)
 matcher('table cell left border', TableCellBorder.like(position='left'))
-
 matcher('table cell top border', TableCellBorder.like(position='top'))
-
 matcher('table cell right border', TableCellBorder.like(position='right'))
-
 matcher('table cell bottom border', TableCellBorder.like(position='bottom'))
 
 matcher('table top border', TableHead
                             / TableRow
                             / TableCell.like(row_index=0)
                             / TableCellBorder.like(position='top'))
-
 matcher('table bottom border', TableBody
                                / TableRow
                                / TableCell.like(row_index=-1)
                                / TableCellBorder.like(position='bottom'))
-
 matcher('table left border', TableCell.like(column_index=0)
                              / TableCellBorder.like(position='left'))
-
 matcher('table right border', TableCell.like(column_index=-1)
                               / TableCellBorder.like(position='right'))
-
-matcher('table head cell left border', TableHead
-                                       / TableRow
-                                       / TableCell
+matcher('table head cell left border', TableHead / TableRow / TableCell
                                        / TableCellBorder.like(position='left'))
-
-matcher('table head cell right border', TableHead
-                                        / TableRow
-                                        / TableCell
+matcher('table head cell right border', TableHead / TableRow / TableCell
                                         / TableCellBorder.like(position='right'))
-
-matcher('table head bottom border', TableHead
-                                    / TableRow
+matcher('table head bottom border', TableHead / TableRow
                                     / TableCell.like(row_index=-1)
                                     / TableCellBorder.like(position='bottom'))
-
-matcher('table head left border', TableHead
-                                  / TableRow
+matcher('table head left border', TableHead / TableRow
                                   / TableCell.like(column_index=0)
                                   / TableCellBorder.like(position='left'))
-
-matcher('table head right border', TableHead
-                                   / TableRow
+matcher('table head right border', TableHead / TableRow
                                    / TableCell.like(column_index=-1)
                                    / TableCellBorder.like(position='right'))
-
-matcher('table body top border', TableBody
-                                 / TableRow
+matcher('table body top border', TableBody / TableRow
                                  / TableCell.like(row_index=0)
                                  / TableCellBorder.like(position='top'))
-
-matcher('table body left border', TableBody
-                                  / TableRow
+matcher('table body left border', TableBody / TableRow
                                   / TableCell.like(column_index=0)
                                   / TableCellBorder.like(position='left'))
-
-matcher('table body right border', TableBody
-                                   / TableRow
+matcher('table body right border', TableBody / TableRow
                                    / TableCell.like(column_index=-1)
                                    / TableCellBorder.like(position='right'))
+
+
 
 matcher('horizontal rule', HorizontalRule)
 
+
+# index
+
 matcher('level 1 index entry', IndexEntry.like('index entry', index_level=1))
-
 matcher('level 2 index entry', IndexEntry.like('index entry', index_level=2))
-
 matcher('level 3 index entry', IndexEntry.like('index entry', index_level=3))
-
 matcher('level 4 index entry', IndexEntry.like('index entry', index_level=4))
