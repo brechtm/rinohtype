a18125b022297ff90b41dff721a36bbf08a0b38a
Brecht Machiels <brecht@mos6581.org>
2016-04-18 11:49:59 +0200

    Docs: do not import the installed rinohtype package

--- doc/conf.py
+++ doc/conf.py
@@ -16,12 +16,12 @@
 import sys
 import os
 
-from rinoh import __version__
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('..'))
+
+from rinoh import __version__
 
 # -- General configuration ------------------------------------------------
 
