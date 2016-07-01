347518019d673c9d1a26049faea74c7adfaa6d11
Brecht Machiels <brecht@mos6581.org>
2016-07-01 16:23:51 +0200

    Fix the pdf reader test (and use pytest)

--- tests/test_pdf_reader.py
+++ tests/test_pdf_reader.py
@@ -1,20 +1,19 @@
 
+import pytest
 
-import unittest
+from io import BytesIO
+
+from rinoh.backend.pdf.reader import PDFObjectReader
 
 
-from pyte.backend.pdf.reader import PDFObjectReader
+def test_read_name():
+    def test_name(bytes_name, unicode_name):
+        reader = PDFObjectReader(BytesIO(bytes_name))
+        name = reader.read_name()
+        assert str(name) == unicode_name
 
-
-class TestPDFReader(unittest.TestCase):
-
-    def test_read_name(self):
-        def test_name(bytes_name, unicode_name):
-            name = read_name(BytesIO(bytes_name))
-            self.assertEqual(str(name), unicode_name)
-
-        test_name(b'Adobe#20Green Blue', 'Adobe Green Blue')
-        test_name(b'PANTONE#205757#20CV', 'PANTONE 5757 CV')
-        test_name(b'paired#28#29parentheses', 'paired()parentheses')
-        test_name(b'The_Key_of_F#23_Minor', 'The_Key_of_F#_Minor')
-        test_name(b'A#42', 'AB')
+    test_name(b'Adobe#20Green', 'Adobe Green')
+    test_name(b'PANTONE#205757#20CV', 'PANTONE 5757 CV')
+    test_name(b'paired#28#29parentheses', 'paired()parentheses')
+    test_name(b'The_Key_of_F#23_Minor', 'The_Key_of_F#_Minor')
+    test_name(b'A#42', 'AB')
