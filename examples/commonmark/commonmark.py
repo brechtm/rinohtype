db44f1f8b17f844bc6ca2880dca3163c249738b1
Brecht Machiels <brecht@mos6581.org>
2016-07-26 12:16:35 +0200

    Fix the CommonMark example

--- examples/commonmark/commonmark.py
+++ examples/commonmark/commonmark.py
@@ -1,14 +1,12 @@
-from rinoh.paper import A4
 from rinoh.backend import pdf
 from rinoh.frontend.commonmark import CommonMarkReader
+from rinoh.templates import Article
 
-from rinohlib.templates.article import Article, ArticleOptions
 
 if __name__ == '__main__':
     reader = CommonMarkReader()
     for name in ('README', ):
         with open(name + '.md', 'rb') as file:
-            flowables = reader.parse(file)
-        article_options = ArticleOptions(page_size=A4, table_of_contents=True)
-        document = Article(flowables, options=article_options, backend=pdf)
+            document_tree = reader.parse(file)
+        document = Article(document_tree, backend=pdf)
         document.render(name)
