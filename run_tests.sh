eeb3383249cb3ce9f583c8cc47d366564f7f1a20
Brecht Machiels <brecht@mos6581.org>
2016-08-11 14:32:10 +0200

    Test sdists with coverage, wheels without

---
+++ run_tests.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# usage:
+#   run_tests.sh [PYTEST_ARGS]
+
+set -e      # exit as soon as one command returns a non-zero exit code
+
+if [[ $WITH_COVERAGE -eq 1 ]]; then
+    COVERAGE_ARGS="--cov=rinoh --cov-report="
+else
+    COVERAGE_ARGS=
+fi
+
+set -x      # echo all lines in the script before executing them
+
+py.test $COVERAGE_ARGS $@
+if [[ $WITH_COVERAGE -eq 1 ]]; then
+    python $TOXINIDIR/coverage.py
+fi
