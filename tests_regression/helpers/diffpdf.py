209ada088a1da6aff3c2a7bedb293ef4b5d5e191
Brecht Machiels <brecht@mos6581.org>
2016-10-27 17:26:08 +0200

    tox: make sure bash (not sh) is used

--- tests_regression/helpers/diffpdf.py
+++ tests_regression/helpers/diffpdf.py
@@ -6,5 +6,5 @@ DIFFPDF_SH = os.path.join(os.path.dirname(__file__), 'diffpdf.sh')
 
 
 def diff_pdf(a_filename, b_filename):
-    rc = subprocess.call(['sh', DIFFPDF_SH, a_filename, b_filename])
+    rc = subprocess.call(['bash', DIFFPDF_SH, a_filename, b_filename])
     return rc == 0
