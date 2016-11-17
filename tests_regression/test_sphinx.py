94f35300bd63ae8e26f6f899abb05b04180d1200
Brecht Machiels <brecht@mos6581.org>
2016-11-17 23:58:35 +0100

    regression tests: test Sphinx inline markup roles

--- tests_regression/test_sphinx.py
+++ tests_regression/test_sphinx.py
@@ -70,3 +70,24 @@ def test_hlist():
               '   * displayed',
               '   * horizontally')
     render(source, 'hlist')
+
+
+def test_inline_markup():
+    source = ('Inline Markup roles:',
+              '',
+              ':abbr:          :abbr:`LIFO (last-in, first-out)` :abbr:`KISS`',
+              ':command:       :command:`rm`',
+              ':dfn:           :dfn:`definition`',
+              ':file:          :file:`README.txt` :file:`Makefile`',
+              ':guilabel:      :guilabel:`&Cancel`',
+              ':kbd:           :kbd:`Control-x`',
+              ':mailheader:    :mailheader:`Content-Type`',
+              ':makevar:       :makevar:`var`',
+              ':manpage:       :manpage:`ls(1)`',
+              ':menuselection: :menuselection:`Menu --> Item`',
+              ':mimetype:      :mimetype:`png`',
+              ':newsgroup:     :newsgroup:`comp.lang.python`',
+              ':program:       :program:`rinoh`',
+              ':regexp:        :regexp:`.*`',
+              ':samp:          :samp:`print 1+{variable}`')
+    render(source, 'inline_markup')
