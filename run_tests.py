f52fbabca1b3ea6d0ae3029681b17553791600cd
Brecht Machiels <brecht@mos6581.org>
2017-02-14 16:18:43 +0100

    Replace run_tests.sh with Python script

---
+++ run_tests.py
@@ -0,0 +1,17 @@
+# usage:
+#   run_tests.py [PYTEST_ARGS]
+
+import os
+import subprocess
+import sys
+
+
+SHELL = sys.platform == 'win32'
+
+
+if __name__ == '__main__':
+    command = ['py.test']
+    if os.environ.get('WITH_COVERAGE') == '1':
+        command.extend(['--cov=rinoh', '--cov-report=xml'])
+    command.extend(sys.argv[1:])
+    subprocess.call(command, shell=SHELL)
