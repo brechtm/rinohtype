2dc2cc2fcbd2aab6e82228789f69c3f0aac2cb38
Brecht Machiels <brecht@mos6581.org>
2017-02-15 11:31:51 +0100

    AppVeyor: publish test output files as artifacts

--- run_tests.py
+++ run_tests.py
@@ -13,6 +13,8 @@ if __name__ == '__main__':
     command = ['py.test']
     if os.environ.get('WITH_COVERAGE') == '1':
         command.extend(['--cov=rinoh', '--cov-report=xml'])
+    if os.environ.get('BASETEMP'):
+        command.append('--basetemp={}'.format(os.environ['BASETEMP']))
     command.extend(sys.argv[1:])
     rc = subprocess.call(command, shell=SHELL)
     raise SystemExit(rc)
