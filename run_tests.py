87fc7420e725d659fbdbae9ecf7b3ed587d39b49
Brecht Machiels <brecht@mos6581.org>
2017-02-27 12:04:32 +0100

    Coverage: print a coverage report to the terminal
    - run in parallel mode and combine the reports so that the report
      contains short file paths (not .tox/.../site-packages/...)
    - also create the XML report from the combined data

--- run_tests.py
+++ run_tests.py
@@ -5,6 +5,8 @@ import os
 import subprocess
 import sys
 
+from coverage import Coverage
+
 
 SHELL = sys.platform == 'win32'
 
@@ -12,9 +14,15 @@ SHELL = sys.platform == 'win32'
 if __name__ == '__main__':
     command = ['py.test']
     if os.environ.get('WITH_COVERAGE') == '1':
-        command.extend(['--cov=rinoh', '--cov-report=xml'])
+        command.extend(['--cov=rinoh', '--cov-report='])
     if os.environ.get('BASETEMP'):
         command.append('--basetemp={}'.format(os.environ['BASETEMP']))
     command.extend(sys.argv[1:])
     rc = subprocess.call(command, shell=SHELL)
+    if os.environ.get('WITH_COVERAGE') == '1':
+        cov = Coverage()
+        cov.load()
+        cov.combine()
+        cov.report(skip_covered=True)
+        cov.xml_report()
     raise SystemExit(rc)
