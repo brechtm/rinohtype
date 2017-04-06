3dd7d390903aa4dae3bdf1aadd8fa1a60617286f
Brecht Machiels <brecht@mos6581.org>
2017-04-06 15:59:58 +0200

    purepng: don't try to load the compiled filters

--- src/rinoh/backend/pdf/xobject/purepng.py
+++ src/rinoh/backend/pdf/xobject/purepng.py
@@ -727,18 +727,6 @@ class BaseFilter(object):
                 result[(4 * i) + j] = row[(3 * i) + j]
 
 
-iBaseFilter = BaseFilter  # 'i' means 'internal'
-try:
-    BaseFilter = _rel_import('pngfilters', 'BaseFilter')
-except:
-    # Whatever happens we could use internal part
-    if not(sys.exc_info()[0] is ImportError):
-        logging.error("Error during import of compiled filters!")
-        logging.error(sys.exc_info()[1])
-        logging.error("Fallback to pure python mode!")
-    BaseFilter = iBaseFilter
-
-
 class Writer(object):
 
     """PNG encoder in pure Python."""
