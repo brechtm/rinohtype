58351d513d1dd06d27afead45ab8c6cbdd6c18e2
Brecht Machiels <brecht@mos6581.org>
2017-07-28 11:40:48 +0200

    Include copyright notice in the unit test files

--- tests/test_color.py
+++ tests/test_color.py
@@ -1,3 +1,9 @@
+# This file is part of rinohtype, the Python document preparation system.
+#
+# Copyright (c) Brecht Machiels.
+#
+# Use of this source code is subject to the terms of the GNU Affero General
+# Public License v3. See the LICENSE file or http://www.gnu.org/licenses/.
 
 
 import pytest
