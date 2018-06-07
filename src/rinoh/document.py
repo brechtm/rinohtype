4a283dfb6a674cf886563a993c0abc5c2e895377
Brecht Machiels <brecht@mos6581.org>
2018-06-07 11:07:38 +0200

    Document.create_outlines(): use show_in_toc()
    - this makes the outline also respect show_in_toc settings
    - Section.show_in_toc() already handles hidden/not-in-toc parents

--- src/rinoh/document.py
+++ src/rinoh/document.py
@@ -462,12 +462,9 @@ to the terms of the GNU Affero General Public License version 3.''')
         current_level = 1
         stack = []
         fake_container = FakeContainer(self)
-        hide_level = float('+inf')
-        for section in (s for s in self._sections if s.level <= hide_level):
-            if section.is_hidden(fake_container):
-                hide_level = section.level
+        for section in self._sections:
+            if not section.show_in_toc(fake_container):
                 continue
-            hide_level = float('+inf')
             section_id = section.get_id(self, create=False)
             section_number = self.get_reference(section_id, 'number')
             section_title = self.get_reference(section_id, 'title')
