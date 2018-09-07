9fd96f2af2bbd0e3776dd563654dbf7ffbbcd396
Jens Heidbüchel <j.heidbuechel@fz-juelich.de>
2018-09-07 10:35:57 +0200

    Use IndexError as parent of NoMoreParentElement
    Use IndexError instead of StopIteration as super class of
    NoMoreParentElement. generators like styled_and_parents should not raise
    StopIteration any more due to PEP479. Changing StopIteration to
    IndexError is imo the most less intrusive way.

--- src/rinoh/style.py
+++ src/rinoh/style.py
@@ -395,7 +395,7 @@ class ContextSelector(Selector):
         return total_score
 
 
-class NoMoreParentElement(StopIteration):
+class NoMoreParentElement(IndexError):
     """The top-level element in the document element tree has been reached"""
 
 
