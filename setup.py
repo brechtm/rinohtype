c73ff67788bd685b7a9eb06747e2b4bc4133f97f
Brecht Machiels <brecht@mos6581.org>
2020-03-06 01:08:19 +0100

    Bump version to 0.4.1.dev

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def get_version():
     from datetime import date
     from subprocess import check_output, CalledProcessError, DEVNULL
 
-    VERSION = '0.4.0'
+    VERSION = '0.4.1.dev'
 
     try:
         is_dirty = check_output(['git', 'status', '--porcelain'],
--- src/rinoh/version.py
+++ src/rinoh/version.py
@@ -1,2 +1,2 @@
-__version__ = '0.4.0'
-__release_date__ = '2020-03-05'
+__version__ = '0.4.1.dev'
+__release_date__ = 'unreleased'
