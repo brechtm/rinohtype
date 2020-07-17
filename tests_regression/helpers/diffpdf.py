3e7df5520646869e609023affecc7a17ad57e634
Brecht Machiels <brecht@mos6581.org>
2020-07-17 11:35:35 +0200

    diffpdf: introduce --split DEPTH argument
    If new chapters start on a fresh page, the diff can be "synchronized"
    at those pages. This will yield more useful output, provided page
    numbers are omitted or restart with each chapter.

--- tests_regression/helpers/diffpdf.py
+++ tests_regression/helpers/diffpdf.py
@@ -8,6 +8,7 @@
 #   or poppler's pdftoppm (known to work: 0.18.4, 0.41.0, 0.85.0, 0.89.0;
 #                          known to fail: 0.42.0)
 
+import argparse
 import os
 import shutil
 import sys
@@ -25,15 +26,39 @@ DIFF_DIR = 'pdfdiff'
 SHELL = sys.platform == 'win32'
 
 
-def diff_pdf(a_filename, b_filename):
-    a_pages = PDFReader(a_filename).catalog['Pages']['Count']
-    b_pages = PDFReader(b_filename).catalog['Pages']['Count']
+def diff_pdf(a_filename, b_filename, depth=None):
+    a = PDFReader(a_filename)
+    b = PDFReader(b_filename)
+    a_numpages = a.catalog['Pages']['Count']
+    b_numpages = b.catalog['Pages']['Count']
 
     success = True
-    if a_pages != b_pages:
+    if a_numpages != b_numpages:
         print('PDF files have different lengths ({} and {})'
-              .format(a_pages, b_pages))
+              .format(int(a_numpages), int(b_numpages)))
         success = False
+
+    sync_points = []
+    if depth is not None:
+        a_pages = list(a.catalog['Pages'].pages)
+        b_pages = list(b.catalog['Pages'].pages)
+        for (a_depth, a_title, a_dest), (b_depth, b_title, b_dest) \
+                in zip(a.iter_outlines(depth), b.iter_outlines(depth)):
+            a_page = a_pages.index(a_dest[0]) + 1
+            b_page = b_pages.index(b_dest[0]) + 1
+            if (a_depth, a_title) != (b_depth, b_title):
+                print("PDF outlines diverge:\n"
+                      "  '{a_title}' -> page {a_page} (depth {a_depth})\n"
+                      "  '{b_title}' -> page {b_page} (depth {b_depth})"
+                      .format(**locals()))
+                success = False
+                break
+            if a_page != b_page:
+                print("Matching PDF outline entries have different targets:\n"
+                      "  '{a_title}' -> page {a_page} / {b_page}"
+                      .format(**locals()))
+                success = False
+                sync_points.append((a_page, b_page))
 
     if os.path.exists(DIFF_DIR):
         for item in os.listdir(DIFF_DIR):
@@ -45,25 +70,35 @@ def diff_pdf(a_filename, b_filename):
     else:
         os.mkdir(DIFF_DIR)
 
-    min_pages = min(a_pages, b_pages)
-    page_numbers = list(range(1, min_pages + 1))
-    # https://pymotw.com/2/multiprocessing/communication.html#process-pools
-    pool_size = cpu_count()
-    pool = Pool(processes=pool_size)
-    print('Running {} processes in parallel'.format(pool_size))
-    perform_diff = partial(diff_page, a_filename, b_filename)
-    try:
-        pool_outputs = pool.map(perform_diff, page_numbers)
-    except CommandFailed as exc:
-        raise SystemExit('Problem running mutool/pdftoppm'
-                         ' or convert (page {})!'.format(exc.page_number))
-    pool.close() # no more tasks
-    pool.join()  # wrap up current tasks
+    min_pages = min(a_numpages, b_numpages)
+    sync_points.append((min_pages, min_pages))
 
-    for page_number, page_diff in zip(page_numbers, pool_outputs):
-        if page_diff != 0:
-            print('page {} ({})'.format(page_number, page_diff))
-            success = False
+    a_start = b_start = 1
+    for a_end, b_end in sync_points:
+        print('Syncing at page {}/{}'.format(a_start, b_start))
+        a_numbers = range(a_start, a_end)
+        b_numbers = range(b_start, b_end)
+        page_numbers = list(zip(a_numbers, b_numbers))
+
+        # https://pymotw.com/2/multiprocessing/communication.html#process-pools
+        pool_size = cpu_count()
+        pool = Pool(processes=pool_size)
+        print('Running {} processes in parallel'.format(pool_size))
+        perform_diff = partial(diff_page, a_filename, b_filename)
+        try:
+            pool_outputs = pool.map(perform_diff, page_numbers)
+        except CommandFailed as exc:
+            raise SystemExit('Problem running mutool/pdftoppm'
+                             ' or convert (page {})!'.format(exc.page_number))
+        pool.close() # no more tasks
+        pool.join()  # wrap up current tasks
+
+        for (a_page, b_page), page_diff in zip(page_numbers, pool_outputs):
+            if page_diff != 0:
+                print('page {}/{} ({})'.format(a_page, b_page, page_diff))
+                success = False
+
+        a_start, b_start = a_end, b_end
     return success
 
 
@@ -72,10 +107,12 @@ class CommandFailed(Exception):
         self.page_number = page_number
 
 
-def diff_page(a_filename, b_filename, page_number):
-    if compare_page(a_filename, b_filename, page_number):
+def diff_page(a_filename, b_filename, ab_page):
+    a_page, b_page = ab_page
+    if compare_page(a_filename, b_filename, a_page, b_page):
         return 0
 
+    page_number = a_page
     diff_jpg_path = os.path.join(DIFF_DIR, '{}.jpg'.format(page_number))
     # http://stackoverflow.com/a/28779982/438249
     diff = Popen(['convert', '-', '(', '-clone', '0-1', '-compose', 'darken',
@@ -95,17 +132,17 @@ def diff_page(a_filename, b_filename, page_number):
     return Decimal(grayscale.stdout.read().decode('ascii'))
 
 
-def compare_page(a_filename, b_filename, page_number):
+def compare_page(a_filename, b_filename, a_page, b_page):
     """Returns ``True`` if the pages at ``page_number`` are identical"""
     compare = Popen(['compare', '-', '-metric', 'AE', 'null:'],
                     shell=SHELL, stdin=PIPE, stdout=DEVNULL, stderr=DEVNULL)
-    a_page = pdf_page_to_ppm(a_filename, page_number, compare.stdin)
+    a_page = pdf_page_to_ppm(a_filename, a_page, compare.stdin)
     if a_page.wait() != 0:
-        raise CommandFailed(page_number)
-    b_page = pdf_page_to_ppm(b_filename, page_number, compare.stdin)
+        raise CommandFailed(a_page)
+    b_page = pdf_page_to_ppm(b_filename, b_page, compare.stdin)
     compare.stdin.close()
     if b_page.wait() != 0:
-        raise CommandFailed(page_number)
+        raise CommandFailed(b_page)
     return compare.wait() == 0
 
 
@@ -135,7 +172,19 @@ else:
     raise SystemExit(2)
 
 
+parser = argparse.ArgumentParser(description='Visual PDF diff.')
+parser.add_argument('file1', metavar='FILE1', type=str,
+                    help='Path to the reference PDF')
+parser.add_argument('file2', metavar='FILE2', type=str,
+                    help='Path of the PDF to diff against the reference PDF')
+parser.add_argument('-s', '--split', metavar='DEPTH', type=int,
+                    help='Reset the diff at the page numbers corresponding '
+                         'to every outline entry up to DEPTH. For example, a '
+                         'DEPTH of 0 limits this to the top-level outlines '
+                         'entries (e.g. chapters).')
+
+
 if __name__ == '__main__':
-    _, a_filename, b_filename = sys.argv
-    rc = 0 if diff_pdf(a_filename, b_filename) else 1
+    args = parser.parse_args()
+    rc = 0 if diff_pdf(args.file1, args.file2, depth=args.split) else 1
     raise SystemExit(rc)
