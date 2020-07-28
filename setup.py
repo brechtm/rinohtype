e6d18049991a60ad77e5ed513e50684a98b526ff
Brecht Machiels <brecht@mos6581.org>
2020-07-28 18:28:18 +0200

    Bump version to 0.4.2

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def get_version():
     from datetime import date
     from subprocess import check_output, CalledProcessError, DEVNULL
 
-    VERSION = '0.4.2.dev'
+    VERSION = '0.4.2'
 
     try:
         is_dirty = check_output(['git', 'status', '--porcelain'],
