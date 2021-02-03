33bc53ad79b4b3634a05db30be5ff92da34c1b3a
Brecht Machiels <brecht@mos6581.org>
2021-02-03 16:39:45 +0100

    Release 0.5.0

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,8 +1,8 @@
 Release History
 ---------------
 
-Release 0.5.0 (upcoming)
-~~~~~~~~~~~~~~~~~~~~~~~~
+Release 0.5.0 (2021-02-03)
+~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 New Features:
 
--- src/rinoh/__init__.py
+++ src/rinoh/__init__.py
@@ -35,7 +35,7 @@ DATA_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'data')
 
 
 __version__ = importlib_metadata.version('rinohtype')
-__release_date__ = 'unreleased'
+__release_date__ = '2021-02-03'
 
 
 from . import resource
