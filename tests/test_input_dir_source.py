e1212e3898647505dc36324854206b958bc8d239
Alex Fargus <alex.fargus@gmail.com>
2021-06-08 20:56:38 +0100

    Unit tests handle Windows paths

--- tests/test_input_dir_source.py
+++ tests/test_input_dir_source.py
@@ -8,6 +8,7 @@
 import pytest
 from os import getcwd
 from os.path import split
+from pathlib import Path
 
 from rinoh.__main__ import InputDirSource
 
@@ -16,13 +17,13 @@ def test_input_source_root_cwd():
     path = "index.rst"
     input_dir, _ = split(path)
     source = InputDirSource(input_dir)
-    assert str(source.root) == getcwd()
-    assert source.location == getcwd()
+    assert source.root == Path.cwd()
+    assert source.location == str(Path.cwd())
 
 
 def test_input_source_root():
     path = "/a/b/index.rst"
     input_dir, _ = split(path)
     source = InputDirSource(input_dir)
-    assert str(source.root) == input_dir
-    assert source.location == input_dir
+    assert source.root == Path(input_dir)
+    assert source.location == str(Path(input_dir))
